85344bbd91d8a2d549a7faae28d5ba7ad8fee600
Sqbika <sqbika@gmail.com>
2021-12-01 08:18:36 +0100

    2021 Start! (Day1)

---
+++ 2021/Solution.kt
@@ -0,0 +1,38 @@
+import java.io.File
+import java.util.*
+
+abstract class Solution(path:String) {
+
+    val input:List<String> by lazy {File(path+"input.txt").readLines()}
+    val test:List<String> by lazy {File(path+"test.txt").readLines()}
+
+    abstract fun part1(input: List<String>)
+
+    abstract fun part2(input: List<String>)
+}
+
+
+fun main(args : Array<String>) {
+    if (args.contains("today")) {
+        val today = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        runDay(today, args.contains("test"))
+    } else {
+        runDay(Integer.parseInt(args[0].replace("day", "")), args.contains("test"))
+    }
+}
+
+fun runDay(today:Int, doTest:Boolean) {
+    val day = Class.forName("day$today.Day$today")
+    val solution:Solution = day.getDeclaredConstructor(String::class.java).newInstance("day$today/") as Solution
+    if (doTest) {
+        solution.part1(solution.test)
+        println("Part1 test")
+        solution.part2(solution.test)
+        println("Part2 test")
+        return
+    }
+    solution.part1(solution.input)
+    println("Part1 done")
+    solution.part2(solution.input)
+    println("Part2 done")
+}
