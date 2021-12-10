694df070dfa5257b404f7a7636c368e523d58736
Sqbika <sqbika@gmail.com>
2021-12-10 08:03:45 +0100

    2021 Day10

---
+++ 2021/day10/Day10.kt
@@ -0,0 +1,83 @@
+package day10
+
+import Solution
+import kotlin.math.floor
+import kotlin.math.max
+
+class Day10(path: String) : Solution(path) {
+
+    override fun part1(input: List<String>) {
+        println("Part 1: ${input.map{ChunkSequence(it).run()}.sumOf { max(it, 0) }}")
+    }
+
+    override fun part2(input: List<String>) {
+        val res = input.map {
+            ChunkSequence(it).run(true)
+        }
+
+        println("Part 2: " +
+            res.filter { it >= 0 }
+                .sorted()
+                .run {
+                    get(size/2)
+                }
+        )
+    }
+}
+
+class ChunkSequence(
+    val seq: String
+) {
+
+    companion object {
+        val closings = mapOf(
+            ")" to 3L,
+            "]" to 57L,
+            "}" to 1197L,
+            ">" to 25137L
+        )
+
+        val openings = mapOf(
+            "(" to ")",
+            "[" to "]",
+            "{" to "}",
+            "<" to ">"
+        )
+
+        val p2Points = mapOf(
+            "(" to 1, "[" to 2, "{" to 3, "<" to 4
+        )
+    }
+
+    fun run (runP2: Boolean = false): Long {
+        val pieces = this.seq.toCharArray().map{it.toString()}.toList()
+
+        val queue:ArrayDeque<String> = ArrayDeque()
+
+        for (it in pieces) {
+            if (openings.containsKey(it)) {
+                queue.add(it)
+            } else {
+                val top = queue.removeLast()
+
+                if (it != openings[top]) {
+                    return if (!runP2)
+                        closings[it]!!
+                    else
+                        -1
+                }
+            }
+        }
+
+        if (!runP2) return 0
+
+        var newScore = 0L
+
+        for (ch in queue.asReversed()) {
+            newScore *= 5
+            newScore += p2Points[ch]!!
+        }
+
+        return newScore
+    }
+}
