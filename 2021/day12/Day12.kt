ca0f9b950449917fd23f27cdaa2404496da92067
Sqbika <sqbika@gmail.com>
2021-12-12 09:08:30 +0100

    2021 Day12

---
+++ 2021/day12/Day12.kt
@@ -0,0 +1,77 @@
+package day12
+
+import Solution
+import java.util.*
+
+class Day12(path: String) : Solution(path) {
+
+    override fun part1(input: List<String>) {
+
+        println("Part 1: ${calculatePaths(input)}")
+    }
+
+    override fun part2(input: List<String>) {
+        println("Part 2: ${calculatePaths(input, true)}")
+    }
+
+    fun calculatePaths(input: List<String>, part2:Boolean = false): Int {
+
+        val paths = mutableMapOf<String, Cave>()
+
+        input.map{
+            it.split("-")
+        }.forEach {
+            for ((a,b) in listOf(Pair(0,1), Pair(1,0))) {
+                if (paths.containsKey(it[a])) {
+                    if (!paths[it[a]]!!.paths.contains(it[b])) paths[it[a]]!!.paths.add(it[b])
+                } else {
+                    paths[it[a]] = Cave(
+                        it[a],
+                        mutableListOf(it[b])
+                    )
+                }
+            }
+        }
+
+        fun groupByCount(stuff: List<String>) = stuff.groupingBy { it }.eachCount()
+
+        val usedPaths = mutableListOf<String>()
+
+        val pathPermute: Queue<MutableList<String>> = LinkedList(listOf(mutableListOf("start")))
+
+        while(pathPermute.isNotEmpty()) {
+            val curPath = pathPermute.poll()
+
+            if (curPath.last() == "end") {
+                val pathString = curPath.joinToString(",")
+
+                if (usedPaths.contains(pathString)) continue
+
+                if (!part2 && groupByCount(curPath.filter{it == it.lowercase()}).any { it.value > 1 }) continue
+
+                usedPaths.add(pathString)
+            } else {
+                val nextPaths = paths[curPath.last()]!!.paths
+
+                nextPaths.forEach { it: String ->
+                    if (
+                        !curPath.contains(it) ||
+                        it == it.uppercase() ||
+                        part2 && it != "start" && it != "end" && groupByCount(curPath.filter{it == it.lowercase()}).all{it.value < 2}) {
+                        pathPermute.add(curPath.toMutableList().apply { add(it) })
+                    }
+                }
+            }
+        }
+
+        return usedPaths.size
+    }
+}
+
+class Cave(
+    val id: String,
+    val paths: MutableList<String> = mutableListOf()
+) {
+
+    val isSmallCave = id == id.lowercase()
+}
